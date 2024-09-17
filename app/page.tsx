ac8d867b1f8d8f6890055721e27423f971c5c12a
Hunter Darling <hunterperryd@gmail.com>
2024-09-17 10:51:52 -0500

    text styling adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <section className="py-24">
         <div className="container max-w-3xl">
           <h1 className="text-2xl font-bold w-fit mx-auto">Howdy!</h1>
-          <p className="text-m font-bold py-6">My name is Hunter Darling, I am a software engineer and I like building things. In fact, I am in the process of building this website. Go figure! Check back soon for the finished product!</p> 
-          <p className="text-m font-bold">In the meantime, if you would like to contact me via LinkedIn or check out my GitHub, please use the links at the bottom of the page.</p>
+          <p className="text-m text-center py-4">My name is Hunter Darling, I am a software engineer and I like building things. In fact, I am in the process of building this website. Go figure! Check back soon for the finished product!</p> 
+          <p className="text-m text-center">In the meantime, if you would like to contact me via LinkedIn or check out my GitHub, please use the links at the bottom of the page.</p>
           <h1 className="text-2xl font-bold w-fit mx-auto py-6">Thanks for stopping by!</h1>
           <div className="w-fit mx-auto">
             <a
