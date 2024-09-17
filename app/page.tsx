7cdec3c25c2f198a81b31d854c22b2a0945ccb59
Hunter Darling <hunterperryd@gmail.com>
2024-09-17 10:33:40 -0500

    add link to Kilroy, styling adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,24 @@ export default function Home() {
   return (
     <>
       <section className="py-24">
-        <div className="container max-w-3xl mx-auto px-24">
-          <h1 className="text-3xl font-bold">Howdy!</h1>
-          <p className="text-sm font-bold py-2">My name is Hunter Darling, I am a software engineer and I like building things. In fact, I am in the process of building this website. Go figure! Check back soon for the finished product!</p> 
-          <p className="text-sm font-bold py-2">In the meantime, if you would like to contact me via LinkedIn or check out my GitHub, please use the links at the bottom of the page.</p>
-          <h1 className="text-3xl font-bold py-2">Thanks for stopping by!</h1>
-          <Image
-            className="mx-auto py-4"
-            src='/images/kilroy.png'
-            alt="Under Construction"
-            height="96"
-            width="102"
-          />
+        <div className="container max-w-3xl">
+          <h1 className="text-2xl font-bold w-fit mx-auto">Howdy!</h1>
+          <p className="text-m font-bold py-6">My name is Hunter Darling, I am a software engineer and I like building things. In fact, I am in the process of building this website. Go figure! Check back soon for the finished product!</p> 
+          <p className="text-m font-bold">In the meantime, if you would like to contact me via LinkedIn or check out my GitHub, please use the links at the bottom of the page.</p>
+          <h1 className="text-2xl font-bold w-fit mx-auto py-6">Thanks for stopping by!</h1>
+          <div className="w-fit mx-auto">
+            <a
+              href="https://en.wikipedia.org/wiki/The_Message_(short_story)" target="_blank"
+              rel="noreferrer noopener">
+              <Image
+                className="mx-auto py-4"
+                src='/images/kilroy.png'
+                alt="Under Construction"
+                height="96"
+                width="96"
+              />
+            </a>
+          </div>
         </div>
       </section>
     </>
