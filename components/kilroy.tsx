9f088957276a09bdda7387845f64ccf927cfc1c7
Hunter Darling <hunterperryd@gmail.com>
2024-09-20 10:30:36 -0500

    update kilroy dimensions

--- components/kilroy.tsx
+++ components/kilroy.tsx
@@ -2,16 +2,16 @@ import Image from "next/image"
 
 export default function Kilroy() {
   return (
-    <div className="w-fit mx-auto">
+    <div className="w-fit mx-auto h-fit">
       <a
         href="https://en.wikipedia.org/wiki/The_Message_(short_story)" target="_blank"
         rel="noreferrer noopener">
         <Image
-          className="mx-auto py-4"
+          className="mx-auto py-4 h-fit"
           src='/images/kilroy.png'
           alt="Kilroy Was Here"
-          height="96"
-          width="96"
+          height="34"
+          width="34"
         />
       </a>
     </div>
