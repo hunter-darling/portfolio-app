551a2fbe461369374e01b1bca299284e6b2767d9
Hunter Darling <hunterperryd@gmail.com>
2025-04-27 17:58:53 -0400

    upload new resume

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="container max-w-3xl">
           <h1 className="text-2xl font-bold w-fit mx-auto">Welcome!</h1>
           <p className="text-l text-center pt-8">My name is Hunter Darling.</p> 
-          <p className="text-m text-center pt-2">I created this portfolio site to serve as a one-stop-shop where you can find my <a className="underline hover:underline-offset-2 hover:text-green-700" href="/files/resume.pdf" target="_blank" rel="noopener noreferrer">CV</a>, see some examples of my <Link className="underline hover:underline-offset-2 hover:text-green-700" href="/projects">work</Link>, and learn a bit <Link className="underline hover:underline-offset-2 hover:text-green-700" href="/about">about</Link> the man behind the monitor.</p>
+          <p className="text-m text-center pt-2">I created this portfolio site to serve as a one-stop-shop where you can find my <Link className="underline hover:underline-offset-2 hover:text-green-700" href="/files/resume.pdf" target="_blank" rel="noopener noreferrer">CV</a>, see some examples of my <Link className="underline hover:underline-offset-2 hover:text-green-700" href="/projects">work</Link>, and learn a bit <Link className="underline hover:underline-offset-2 hover:text-green-700" href="/about">about</Link> the man behind the monitor.</p>
           <p className="text-m text-center pt-2">If you would like to contact me directly, feel free to send me a message on <a
               className="underline hover:underline-offset-2 hover:text-green-700"
               href="https://linkedin.com/in/hunter-darling-55676b106"
